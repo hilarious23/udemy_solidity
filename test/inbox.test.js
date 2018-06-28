5c6c188c7ef50f896005e405b3290787da15847d
hilarious23 <s.positex11@gmail.com>
2018-06-29 08:39:55 +0900

    Fetching Accounts from Ganache and refactoring

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -3,7 +3,22 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3 = new Web3(ganache.provider());
 
+let accounts;
 
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts()
+
+  // Use one of those accounts to deploy the contract
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    console.log(accounts);
+  });
+});
+
+/*
 class Car {
   park() {
     return 'stopped';
@@ -29,3 +44,4 @@ describe('Car',  () => {
     assert.equal(car.drive(), 'vroom');
   });
 });
+*/
