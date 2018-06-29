30c6d41b8a96f5af73e4f1668a0e693f9b75efa1
hilarious23 <s.positex11@gmail.com>
2018-06-29 22:45:29 +0900

    Deployment to Rinkby

--- test/inbox.test.js
+++ test/inbox.test.js
@@ -20,8 +20,6 @@ beforeEach(async () => {
   inbox = await new web3.eth.Contract(JSON.parse(interface))
     .deploy({ data: bytecode, arguments: ['Hi there!'] })
     .send({ from: accounts[0], gas: '1000000' });
-
-  // ADD THIS ONE LINE RIGHT HERE!!!!! <---------------------
   inbox.setProvider(provider);
 });
 
