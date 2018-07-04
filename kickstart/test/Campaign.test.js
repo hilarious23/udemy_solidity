55e566ad117df311107aed4d6c97618d9d225402
hilarious23 <s.positex11@gmail.com>
2018-07-05 02:28:23 +0900

    End to End testing

--- kickstart/test/Campaign.test.js
+++ kickstart/test/Campaign.test.js
@@ -59,4 +59,57 @@ describe('Campaigns', () => {
     //if isContributor is false, test fails
     assert(isContributor);
   });
+
+  it('requires a minimum contribution', async () => {
+    //わざとminimum以下でsend。falseでたらtestとしては正しいというコード
+    try {
+      await campaign.methods.contribute().send({
+        value: '5',
+        from: account[1]
+      });
+      assert(false);
+    } catch (err) {
+      assert(err);
+    }
+  });
+  it('allows a manager to make a payment request', async () => {
+    await campaign.methods
+      .createRequest('Buy batteries', '100', accounts[1])
+      .send({
+        from: accounts[0],
+        gas: '1000000'
+      });
+    //createRequestされたので、それを確認
+    const request = await campaign.methods.requests(0).call();
+
+    assert.equal('Buy batteries', request.description);
+  });
+
+  it('processes requests', async () => {
+    await campaign.methods.contribute().send({
+      from: accounts[0],
+      value: web3.utils.toWei('10', 'ether')
+    });
+
+    await campaign.methods
+      .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
+      .send({ from: accounts[0], gas:'1000000' });
+
+    await campaign.methods.approveRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    await campaign.methods.finalizeRequest(0).send({
+      from: accounts[0],
+      gas: '1000000'
+    });
+
+    let balance = await web3.eth.getBalance(accounts[1]);
+    balance = web3.utils.fromWei(balance, 'ether');
+    //parseFloat takes a string and tries to turn it into a decimal number
+    balance = parseFloat(balance);
+    console.log(balance);
+    assert(balance > 104)
+  });
 });
