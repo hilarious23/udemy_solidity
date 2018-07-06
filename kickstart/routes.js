87f6a7a7fa06b6b5e41163a6735a476109d25e90
hilarious23 <s.positex11@gmail.com>
2018-07-06 14:10:24 +0900

    Lecture 194 Routing to new request

--- kickstart/routes.js
+++ kickstart/routes.js
@@ -4,6 +4,7 @@ routes
 //  Aのとき、Bを表示。new設定しないとnewとaddress勘違いする
   .add('/campaigns/new', '/campaigns/new')
   .add('/campaigns/:address', '/campaigns/show')
-  .add('/campaigns/:address/requests', '/campaigns/requests/index');
+  .add('/campaigns/:address/requests', '/campaigns/requests/index')
+  .add('/campaigns/:address/requests/new', '/campaigns/requests/new');
 
 module.exports = routes;
