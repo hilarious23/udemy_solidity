d4fbc9ff847f08dda9e8f0a3a714045f0b86a14e
hilarious23 <s.positex11@gmail.com>
2018-07-07 01:19:11 +0900

    The end of this course

--- kickstart/ethereum/factory.js
+++ kickstart/ethereum/factory.js
@@ -3,7 +3,7 @@ import CampaignFactory from './build/CampaignFactory.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(CampaignFactory.interface),
-  '0x7138Fa3a8d3f2f26b88c80dE72D3450d11f2c110'
+  '0xd4A8E021aeaF4a5A1723B52821aE0F32194e0874'
 );
 
 export default instance;
