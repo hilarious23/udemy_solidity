9857769eb8cde4a345154865dca7f055eda6799b
hilarious23 <s.positex11@gmail.com>
2018-07-06 13:58:33 +0900

    Lecture 193 until Routing to request list

--- kickstart/routes.js
+++ kickstart/routes.js
@@ -3,6 +3,7 @@ const routes = require('next-routes')();
 routes
 //  Aのとき、Bを表示。new設定しないとnewとaddress勘違いする
   .add('/campaigns/new', '/campaigns/new')
-  .add('/campaigns/:address', '/campaigns/show');
+  .add('/campaigns/:address', '/campaigns/show')
+  .add('/campaigns/:address/requests', '/campaigns/requests/index');
 
 module.exports = routes;
